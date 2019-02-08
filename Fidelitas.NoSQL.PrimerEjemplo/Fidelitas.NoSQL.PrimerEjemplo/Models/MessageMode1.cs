8ba1f9425da9e097d2c8668a435e7103c37fdfe4
sebastian bermudez <sebas-bermudez15@hotmail.com>
2019-02-07 20:22:33 -0600

    Primera Seción

---
+++ Fidelitas.NoSQL.PrimerEjemplo/Fidelitas.NoSQL.PrimerEjemplo/Models/MessageMode1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Fidelitas.NoSQL.PrimerEjemplo.Models
+{
+    public class MessageMode1
+    {
+        public string From { get; set; }
+
+        public string To { get; set; }
+
+        public string Message { get; set; }
+    }
+}
