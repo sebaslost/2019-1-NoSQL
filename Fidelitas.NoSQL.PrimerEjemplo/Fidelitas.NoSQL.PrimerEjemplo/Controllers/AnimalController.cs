7ff85ea1c313e1b0c5d5f58a5811397fbd4b2fa7
sebastian bermudez <sebas-bermudez15@hotmail.com>
2019-02-21 20:12:25 -0600

    commitSem5

---
+++ Fidelitas.NoSQL.PrimerEjemplo/Fidelitas.NoSQL.PrimerEjemplo/Controllers/AnimalController.cs
@@ -0,0 +1,99 @@
+﻿using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Fidelitas.NoSQL.PrimerEjemplo.Controllers
+{
+    public class AnimalController : Controller
+    {
+        //ctor+dobletap crea el metodo
+        public AnimalController()
+        {
+            var server = new MongoClient();
+        }
+
+
+
+
+        // GET: Animal
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Animal/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Animal/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Animal/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Animal/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Animal/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Animal/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Animal/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
